0312ad2222de2f5bf668f2f4c88a920504f3411b
VatanakChamroeun <ch.vatanak01@gmail.com>
2025-10-18 01:15:57 +0700

    feat: implement customizable dialog style options
    Close #9

--- lib/flutter_custom_updater.dart
+++ lib/flutter_custom_updater.dart
@@ -1,7 +1,8 @@
 // Main export file for flutter_custom_updater package
 
 export 'src/app_updater.dart';
+export 'src/models/dialog_style.dart';
 export 'src/models/update_info.dart';
 export 'src/models/updater_config.dart';
-export 'src/widgets/update_dialog.dart';
 export 'src/widgets/download_dialog.dart';
+export 'src/widgets/update_dialog.dart';
